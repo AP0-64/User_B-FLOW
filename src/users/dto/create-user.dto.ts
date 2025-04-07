dfce5280a6365a160b023902bfc7d75505488578
Paolo BERTINI <bertin_p@etna-alternance.net>
2025-04-07 02:37:00 +0200

    post

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty()
+  first_name: string;
+
+  @ApiProperty()
+  last_name: string;
+
+  @ApiProperty()
+  mail: string;
+
+  @ApiProperty()
+  password: string;
+}
