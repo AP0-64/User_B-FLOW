b5a459e93fe0ac41a1efc6754c0346115739a29a
Paolo BERTINI <bertin_p@etna-alternance.net>
2025-04-07 02:57:17 +0200

    Update Swagger response types

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { usertable } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserEntity implements usertable {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  first_name: string;
+
+  @ApiProperty()
+  last_name: string;
+
+  @ApiProperty()
+  mail: string;
+
+  @ApiProperty()
+  password: string;
+}
